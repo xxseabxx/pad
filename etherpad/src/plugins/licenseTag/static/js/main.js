15436e038dfe90a5c8e9c16777866db8d892ea2a
xxseabxx <s.hochgatterer@web.de>
2011-03-01 16:57:37 +0100

    test

--- etherpad/src/plugins/licenseTag/static/js/main.js
+++ etherpad/src/plugins/licenseTag/static/js/main.js
@@ -2,6 +2,29 @@ function licenseTagInit() {
   this.hooks = [];
   this.licenseTagClicked = licenseTagClicked;
   this.licenseTagSelectLicenseClicked = licenseTagSelectLicenseClicked;
+    
+  // Objekt erzeugen
+    // hier ohne Fallunterscheidung, da für das Beispiel einfacher
+    var xmlHttpObject = new XMLHttpRequest();
+	
+    // Funktion, die bei Statusänderungen reagiert
+    function handleStateChange()
+    {
+        // Derzeitigen Status zurückgeben
+        alert("xmlHttpObject.readyState = " + xmlHttpObject.readyState + " HTTP-Status = " + xmlHttpObject.status );
+		alert("Ergebnis:" +xmlHttpObject.responseText);
+    }
+    
+    // Wenn Dokument geladen ausführen
+    window.onload = function() {
+        // Anfrage vorbereiten, ruft www.webmasterpro.de auf
+
+        xmlHttpObject.open('GET', 'https://api.zotero.org/users/188286/collections/ZUD7JW6D/items?apiKey=3atnk1bqyhdej89qlapv2ueq&format=bib&style=apa',true);
+        // Handler hinterlegen
+        xmlHttpObject.onreadystatechange = handleStateChange;
+        // Anfrage abschicken
+        xmlHttpObject.send();
+    }
 }
 
 function licenseTagClicked () {
