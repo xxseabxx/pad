c5e6e9f6264508acf7f94fbcaa42df532a1921ab
xxseabxx <s.hochgatterer@web.de>
2011-03-01 17:04:30 +0100

    hgv

--- etherpad/src/plugins/licenseTag/static/js/main.js
+++ etherpad/src/plugins/licenseTag/static/js/main.js
@@ -1,35 +1,34 @@
+
+   var xmlHttpObject = new XMLHttpRequest();
 function licenseTagInit() {
   this.hooks = [];
   this.licenseTagClicked = licenseTagClicked;
   this.licenseTagSelectLicenseClicked = licenseTagSelectLicenseClicked;
-    
+
+}
+
+function licenseTagClicked () {
+  $('#licenseTag-license-selector').toggle();
+  
+  
+      
   // Objekt erzeugen
     // hier ohne Fallunterscheidung, da für das Beispiel einfacher
-    var xmlHttpObject = new XMLHttpRequest();
-	
+ 
     // Funktion, die bei Statusänderungen reagiert
+        xmlHttpObject.open('GET', 'https://api.zotero.org/users/188286/collections/ZUD7JW6D/items?apiKey=3atnk1bqyhdej89qlapv2ueq&format=bib&style=apa',true);
+        // Handler hinterlegen
+        xmlHttpObject.onreadystatechange = handleStateChange;
+        // Anfrage abschicken
+        xmlHttpObject.send();
+    
+}
     function handleStateChange()
     {
         // Derzeitigen Status zurückgeben
         alert("xmlHttpObject.readyState = " + xmlHttpObject.readyState + " HTTP-Status = " + xmlHttpObject.status );
 		alert("Ergebnis:" +xmlHttpObject.responseText);
     }
-    
-    // Wenn Dokument geladen ausführen
-    window.onload = function() {
-        // Anfrage vorbereiten, ruft www.webmasterpro.de auf
-
-        xmlHttpObject.open('GET', 'https://api.zotero.org/users/188286/collections/ZUD7JW6D/items?apiKey=3atnk1bqyhdej89qlapv2ueq&format=bib&style=apa',true);
-        // Handler hinterlegen
-        xmlHttpObject.onreadystatechange = handleStateChange;
-        // Anfrage abschicken
-        xmlHttpObject.send();
-    }
-}
-
-function licenseTagClicked () {
-  $('#licenseTag-license-selector').toggle();
-}
 
 function licenseTagSelectLicenseClicked(license) {
   padeditor.ace.replaceRange(undefined, undefined, " #cite:" + license + " ");
