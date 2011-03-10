2f36a3406919f97302fd1fbff647e94031c78cb4
xxseabxx <s.hochgatterer@web.de>
2011-03-10 10:50:11 +0100

    add

--- etherpad/src/plugins/langTag/static/js/main.js
+++ etherpad/src/plugins/langTag/static/js/main.js
@@ -2,10 +2,18 @@ function langTagInit() {
   this.hooks = [];
   this.langTagClicked = langTagClicked;
   this.langTagSelectLangClicked = langTagSelectLangClicked;
+  var path = 'https://api.zotero.org/users/188286/collections/ZUD7JW6D/items?apiKey=3atnk1bqyhdej89qlapv2ueq&format=bib&style=apa';
+	
+	requestCrossDomain(path, function(results) {
+		alert("hallo");
+	});
+  
 }
 
 function langTagClicked () {
+  
   $('#langTag-language-selector').toggle();
+  
 }
 
 function langTagSelectLangClicked(lang) {
@@ -13,7 +21,31 @@ function langTagSelectLangClicked(lang) {
   padeditor.ace.focus();
   $('#langTag-language-selector').toggle();
 }
+function requestCrossDomain( site, callback ) {
 
+	if ( !site ) {
+		alert('No site was passed.');
+		return false;
+	}
+
+	var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=?';
+	$.getJSON( yql, cbFunc );
+	
+	function cbFunc(data) {
+	// If we have something to work with...
+	if ( data.results[0] ) {
+		// Strip out all script tags, for security reasons.
+		// BE VERY CAREFUL. This helps, but we should do more. 
+		data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '');
+
+		if ( typeof callback === 'function') {
+			callback(data);
+		}
+	}
+	
+	else throw new Error('Nothing returned from getJSON.');
+	}
+}
 
 /* used on the client side only */
 langTag = new langTagInit();
